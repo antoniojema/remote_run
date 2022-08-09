81178ad4da15adac21cfaaddca0b3e7e38c197d4
Antonio <antoniojema@gmail.com>
2022-08-09 10:56:10 +0200

    Minor

--- common.py
+++ common.py
@@ -3,4 +3,3 @@ from pathlib import Path
 __STDOUT__   : Path = Path(__file__).parent / "stdout.txt"
 __RUN_PATH__ : Path = Path(__file__).parent / "run.json"
 __RUN_LIST__ : list = []
-__CURRENT_ID__ : int = 0
